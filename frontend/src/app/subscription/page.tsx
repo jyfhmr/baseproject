f4ece2fb4d612004e04610b48889f7a3c5988c4b
Gohan3500 <carlosbetancourt419@gmail.com>
2024-11-12 10:14:45 -0400

    checkout

--- frontend/src/app/subscription/page.tsx
+++ frontend/src/app/subscription/page.tsx
@@ -1,185 +1,102 @@
-'use client';
+"use client"; // Agrega esto para habilitar el uso de hooks
 
-import { useState, useEffect } from 'react';
-import { useParams, useRouter } from 'next/navigation';
-import { Form, Input, Select, Button, Col, Row } from 'antd';
-import ButtonSubmit from '@/components/dashboard/ButtonSubmit';
-import SetPageInfo from '@/components/dashboard/SetPageInfo';
-import Loading from '@/components/Loading';
-import { createOrUpdate, getOne, getAllData } from '@/services';
-import { AppstoreOutlined } from '@ant-design/icons';
-import { toast } from 'react-toastify';
-import './subscription.css'; // Importa el archivo CSS aquí
+import { useState } from 'react';
+import { Form, Input, Select, Button, Col, Row, Radio } from 'antd';
+import { CreditCardOutlined, MobileOutlined } from '@ant-design/icons';
+import { RadioChangeEvent } from 'antd/es/radio';
+import './subscription.css';
 
-type FieldType = {
-    name: string;
-    email: string;
-    password: string;
-    contraseña2?: string;
-    file?: any;
-    fullName: string;
-    phoneNumber: string;
-    dni: string;
-    profile: number;
-};
-
-const moduleName = 'config/users';
-
-const ModuleForm = () => {
-    const [loader, setLoader] = useState(false);
-    const [spinner, setSpinner] = useState(true);
-    const [data, setData] = useState<Partial<FieldType>>({});
-    const [profiles, setProfiles] = useState<{ id: number; name: string }[]>([]);
+const PaymentForm = () => {
     const [form] = Form.useForm();
-    const params = useParams();
-    const id = params?.id ? Number(params.id) : null;
-    const router = useRouter();
+    const [paymentMethod, setPaymentMethod] = useState<'credito' | 'pagoMovil'>('pagoMovil'); // Inicializamos con 'pagoMovil'
 
-    const getProfiles = async () => {
-        try {
-            const response = await getAllData("config/profiles");
-            setProfiles(response.data);
-        } catch (error) {
-            console.error("Error al cargar perfiles:", error);
-        }
-    };
-
-    const getDataForUser = async () => {
-        try {
-            if (id !== null) {
-                const get = await getOne(moduleName, id);
-                const profileId = get.profile ? get.profile.id : null;
-                setData({ ...get, profile: profileId });
-            }
-            setSpinner(false);
-        } catch (error) {
-            console.error("Error al obtener datos del usuario:", error);
-        }
-    };
-
-    useEffect(() => {
-        getProfiles();
-        if (id !== null) {
-            getDataForUser();
-        } else {
-            setSpinner(false);
-        }
-    }, [id]);
-
-    const pageName = `${id ? 'Editar' : 'Nuevo'} Usuario`;
-    const iconPage = <AppstoreOutlined />;
-    const breadCrumb = [{ title: 'Configuración' }, { title: 'Usuario' }, { title: pageName }];
-
-    const onFinish = async (values: FieldType) => {
-        delete values.contraseña2;
-        try {
-            setLoader(true);
-            const res = await createOrUpdate(moduleName, values, id || undefined);
-            if (res?.statusCode === 401) {
-                toast.error(res.message);
-                setLoader(false);
-                return;
-            }
-            toast.success(`Usuario ${id ? 'editado' : 'creado'} con éxito`);
-        } catch (error) {
-            setLoader(false);
-            toast.error('Ha ocurrido un error');
-        }
+    const handlePaymentMethodChange = (e: RadioChangeEvent) => {
+        setPaymentMethod(e.target.value);
     };
 
     const handleClearPaymentFields = () => {
-        form.resetFields(['bank', 'phone', 'amount', 'concept', 'dni']);
+        form.resetFields();
     };
 
-    return spinner ? (
-        <Loading />
-    ) : (
-        <div className="subscription-container">
-            {/* Apartado de Registro */}
-            <div className="subscription-registration">
-                <SetPageInfo pageName={pageName} iconPage={iconPage} breadCrumb={breadCrumb} />
-                <h3>Datos del Usuario</h3>
-                    <Form
-                        name="wrap"
-                        colon={false}
-                        layout="vertical"
-                        onFinish={onFinish}
-                        initialValues={data}
-                    >
-                        <Row gutter={[16, 16]}>
-                            <Col span={24}>
-                                <Form.Item label="Nombre Completo" name="fullName" rules={[{ required: true, message: 'Este campo es obligatorio' }]}>
-                                    <Input />
-                                </Form.Item>
-                            </Col>
-                            <Col span={24}>
-                                <Form.Item label="Nombre de Usuario" name="name" rules={[{ required: true, message: 'Este campo es obligatorio' }]}>
-                                    <Input />
-                                </Form.Item>
-                            </Col>
-                            <Col span={24}>
-                                <Form.Item label="Correo electrónico" name="email" rules={[{ required: true, message: 'Este campo es obligatorio' }, { type: "email", message: 'Ingresa un correo electrónico válido' }]}>
-                                    <Input />
-                                </Form.Item>
-                            </Col>
-                            <Col span={24}>
-                                <Form.Item label="Número de Teléfono" name="phoneNumber" rules={[{ required: true, message: 'Este campo es obligatorio' }]}>
-                                    <Input />
-                                </Form.Item>
-                            </Col>
-                            <Col span={24}>
-                                <Form.Item label="Documento de identidad" name="dni" rules={[{ required: true, message: 'Este campo es obligatorio' }]}>
-                                    <Input />
-                                </Form.Item>
-                            </Col>
-                            {!id && (
-                                <>
-                                    <Col span={24}>
-                                        <Form.Item label="Contraseña" name="password" rules={[{ required: true, message: 'Este campo es obligatorio' }, { min: 6, message: 'La contraseña debe tener al menos 6 caracteres' }]}>
-                                            <Input.Password />
-                                        </Form.Item>
-                                    </Col>
-                                    <Col span={24}>
-                                        <Form.Item label="Repetir Contraseña" name="contraseña2" dependencies={["password"]} rules={[{ required: true, message: 'Este campo es obligatorio' }, ({ getFieldValue }) => ({ validator(_, value) { if (!value || getFieldValue('password') === value) { return Promise.resolve(); } return Promise.reject(new Error('Las contraseñas no coinciden')); }, })]} >
-                                            <Input.Password />
-                                        </Form.Item>
-                                    </Col>
-                                </>
-                            )}
-                        </Row>
-                        <ButtonSubmit loader={loader}>{!id ? 'Registrar' : 'Editar'}</ButtonSubmit>
-                    </Form>
-            </div>
+    return (
+        <div className="checkout-container">
+            <h2 className="payment-title">Seleccione su método de pago</h2>
 
-            {/* Apartado de Pago */}
-            <div className="subscription-payment">
-                <h3>Datos del Pago Movil</h3>
-                <Form form={form} layout="vertical">
-                    <Form.Item label="Banco" name="bank" rules={[{ required: true, message: 'Este campo es obligatorio' }]}>
-                        <Select placeholder="Selecciona un banco">
-                            <Select.Option value="0102">0102 - BANCO DE VENEZUELA</Select.Option>
-                        </Select>
-                    </Form.Item>
-                    <Form.Item label="Documento de identidad" name="dni" rules={[{ required: true, message: 'Este campo es obligatorio' }]}>
-                        <Input />
-                    </Form.Item>
-                    <Form.Item label="Teléfono" name="phone" rules={[{ required: true, message: 'Este campo es obligatorio' }]}>
-                        <Input />
-                    </Form.Item>
-                    <Form.Item label="Monto" name="amount" rules={[{ required: true, message: 'Este campo es obligatorio' }]}>
-                        <Input />
-                    </Form.Item>
-                    <Form.Item label="Concepto" name="concept">
-                        <Input />
-                    </Form.Item>
-                    <div className="subscription-buttons">
-                        <Button type="primary">Pagar</Button>
-                        <Button onClick={handleClearPaymentFields}>Limpiar</Button>
-                    </div>
-                </Form>
+            <div className="payment-form">
+            <Radio.Group
+  onChange={handlePaymentMethodChange}
+  value={paymentMethod}
+  className="payment-methods"
+  buttonStyle="solid" // Añadido para que mantenga el estilo en línea
+        >
+        <Radio.Button value="credito">
+            <CreditCardOutlined /> Crédito
+        </Radio.Button>
+        <Radio.Button value="pagoMovil" className="payment-option">
+            Pago móvil
+            <img src="/img/pagomovil.png" className="payment-icon" />
+        </Radio.Button>
+        </Radio.Group>
+                {paymentMethod && (
+                    <Form form={form} layout="vertical" className="payment-form-fields">
+                        {paymentMethod === 'credito' && (
+                            <>
+                                <Form.Item label="Número de Tarjeta" name="cardNumber" rules={[{ required: true }]}>
+                                    <Input />
+                                </Form.Item>
+                                <Form.Item label="Titular de la Tarjeta" name="cardHolder" rules={[{ required: true }]}>
+                                    <Input />
+                                </Form.Item>
+                                <Row gutter={16}>
+                                    <Col span={12}>
+                                        <Form.Item label="Fecha de Expiración" name="expiryDate" rules={[{ required: true }]}>
+                                            <Input placeholder="MM/AA" />
+                                        </Form.Item>
+                                    </Col>
+                                    <Col span={12}>
+                                        <Form.Item label="CVC" name="cvc" rules={[{ required: true }]}>
+                                            <Input />
+                                        </Form.Item>
+                                    </Col>
+                                </Row>
+                            </>
+                        )}
+                        {paymentMethod === 'pagoMovil' && (
+                            <>
+                                <Form.Item label="Operación" name="operation" rules={[{ required: true }]}>
+                                    <Select defaultValue="Personas">
+                                        <Select.Option value="Personas">Personas</Select.Option>
+                                    </Select>
+                                </Form.Item>
+                                <Form.Item label="Documento" name="document" rules={[{ required: true }]}>
+                                    <Input />
+                                </Form.Item>
+                                <Form.Item label="Banco" name="bank" rules={[{ required: true }]}>
+                                    <Select defaultValue="0102 - BANCO DE VENEZUELA">
+                                        <Select.Option value="0102 - BANCO DE VENEZUELA">0102 - BANCO DE VENEZUELA</Select.Option>
+                                    </Select>
+                                </Form.Item>
+                                <Form.Item label="Teléfono" name="phone" rules={[{ required: true }]}>
+                                    <Input />
+                                </Form.Item>
+                                <Form.Item label="Monto" name="amount" rules={[{ required: true }]}>
+                                    <Input />
+                                </Form.Item>
+                                <Form.Item label="Concepto" name="concept" rules={[{ required: true }]}>
+                                    <Input />
+                                </Form.Item>
+                            </>
+                        )}
+
+                        <div className="payment-buttons">
+                            <Button type="primary" htmlType="submit">Pagar</Button>
+                            <Button onClick={handleClearPaymentFields}>Limpiar</Button>
+                        </div>
+                    </Form>
+                )}
             </div>
         </div>
     );
 };
 
-export default ModuleForm;
+export default PaymentForm;
